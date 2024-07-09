57b75c2232d44bbc28281fb91987b508c310567e
gülsüm nur <114823492+gnurm7@users.noreply.github.com>
2024-07-09 16:19:23 +0300

    .

--- djangoProject1/asgi.py
+++ djangoProject1/asgi.py
@@ -1,21 +1,14 @@
 # mysite/asgi.py
 import os
-import chat.routing
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject1.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
-        ),
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
     }
 )
--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from channels_redis.core import RedisChannelLayer
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
